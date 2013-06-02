0074ca8da6013bb016aadb4d49cba20437d9fd72
Jordan Byron <jordan.byron@gmail.com>
2013-06-02 09:34:58 -0400

    Use proper check out numbers and better variable names
    Previously, the "Total Check Outs" number was labeled for the entire
    clinic but in fact was only reporting on the number of check outs for
    today. This resolves that bug and uses better variable names from MOMMA.

--- jobs/patients.rb
+++ jobs/patients.rb
@@ -3,10 +3,14 @@ require_relative '../lib/momma/client'
 SCHEDULER.every '15s', allow_overlapping: false do |job|
   patients = Momma::Client.patients
 
-  send_event('patients-total',         { current: patients['total'] })
-  send_event('patients-today',         { current: patients['today'] })
-  send_event('check-outs-per-hour',    { value:   patients['check_outs_per_hour'] })
-  send_event('registrations-per-hour', { value:   patients['per_hour'] })
+  send_event('registrations-total', { current: patients['registrations_total'] })
+  send_event('registrations-today', { current: patients['registrations_today'] })
 
-  send_event('patients-in-clinic',     { current: patients['in_clinic'] })
+  send_event('check-outs-total', { current: patients['check_outs_total'] })
+  send_event('check-outs-today', { current: patients['check_outs_today'] })
+
+  send_event('check-outs-per-hour',    { value: patients['check_outs_per_hour'] })
+  send_event('registrations-per-hour', { value: patients['registrations_per_hour'] })
+
+  send_event('patients-in-clinic', { current: patients['in_clinic'] })
 end
--- jobs/summary.rb
+++ jobs/summary.rb
@@ -1,10 +1,9 @@
 require_relative '../lib/momma/client'
 
-SCHEDULER.every '1m', allow_overlapping: false do |job|
+SCHEDULER.every '30m', allow_overlapping: false do |job|
   summary = Momma::Client.summary
 
   send_event('total-donated', { current: summary["total_donated"] })
   send_event('procedures', { current: summary["procedures"] })
-  send_event('checkouts', { current: summary["check_outs"] })
   send_event('top-procedures', { items: summary["top_procedures"] })
 end
